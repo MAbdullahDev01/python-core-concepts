59f6d627789321f1d8548b93d50db654399d7035
MuhammadAbdullah-coder997 <abdullahiqbal2008@gmail.com>
2026-06-06 21:56:54 +0500

    exercise 2 complete for day 4

--- week_1/day_4/day.py
+++ week_1/day_4/day.py
@@ -12,8 +12,32 @@
 
 # Create a second file named main.py in the same folder. Write import utility and run main.py. Does it print the greeting? Explain why or why not based on how __name__ works.
 
+from tkinter.filedialog import Directory
+
 from utility import greet_user
 
 greet_user("developer")
 
 # the greeting wasn't printed when ran from day.py because __name__ only runs when the script is ran from the original file
+
+# Exercise 2: The Automated Folder Cleaner
+# Write a script that helps organize a messy computer directory.
+
+# Import Path from pathlib.
+
+# Define a path to a new directory called Project_Backup inside your current working directory.
+
+# Check if Project_Backup already exists. If it doesn't, use your script to create the folder.
+
+# Inside that new folder, create three empty text files programmatically named: notes.txt, todo.txt, and logs.txt.
+
+from pathlib import Path 
+
+directory = Path("D:/projects/python-core-concepts/week_1/day_4")
+q = directory / "Project_Backup" 
+if not q.exists(): q.mkdir()
+
+file_paths = ["notes.txt", "todo.txt", "logs.txt"]
+for file in file_paths:
+    file_path = q / file
+    file_path.touch()
